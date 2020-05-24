540cf79a539ff0f484a9a375fd02d6590816d927
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-05-23 21:13:21 -0400

    learning more variable types; done with course for now

--- src/CharExamples.java
+++ src/CharExamples.java
@@ -5,9 +5,14 @@ public class CharExamples {
         char myUnicode = '\u0041';
         char myUnicodeSupplement = '\u00AE';
 
+        boolean isWeekday  = false;
+        boolean isNight = true;
+
         System.out.println("Hello World!");
         System.out.println("myChar: " + myChar);
         System.out.println("myUnicode: " + myUnicode);
         System.out.println("myUnicodeSupplement: " + myUnicodeSupplement);
+        System.out.println("isWeekday: " + isWeekday);
+        System.out.println("isNight: " + isNight);
     }
 }
