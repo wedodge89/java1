f26a4cc8032e3a59d8e4271b9a4ffef74f111f33
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-05-23 21:03:07 -0400

    experimenting with more variable types

---
+++ src/CharExamples.java
@@ -0,0 +1,13 @@
+public class CharExamples {
+
+    public static void main(String[] args) {
+        char myChar = 'x';
+        char myUnicode = '\u0041';
+        char myUnicodeSupplement = '\u00AE';
+
+        System.out.println("Hello World!");
+        System.out.println("myChar: " + myChar);
+        System.out.println("myUnicode: " + myUnicode);
+        System.out.println("myUnicodeSupplement: " + myUnicodeSupplement);
+    }
+}
